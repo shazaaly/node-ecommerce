73436aec4be400def60fae1a76b6039fd1a8cb17
shazaaly <shaza.aly@gmail.com>
2024-03-18 02:33:37 +0200

    Add error handling and response status codes to email sending functionality

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -27,8 +27,10 @@ const sendMail = expressAsyncHandler(async (req, res) => {
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
             console.log(error);
+            res.status(500).send('Error sending email');
         } else {
             console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent: ' + info.response);
         }
     });
     
