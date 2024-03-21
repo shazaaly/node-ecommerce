48bc1693020156db04473282669e818536df86eb
shazaaly <shaza.aly@gmail.com>
2024-03-21 21:50:32 +0200

    Add Swagger API documentation to productRouter.js

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,51 +1,205 @@
 const express = require('express');
 const router = express.Router();
-const {isAdmin, authMiddleware} = require('../middlewares/authorizationMiddleware')
-const swaggerJsdoc = require('swagger-jsdoc');
+const { isAdmin, authMiddleware } = require('../middlewares/authorizationMiddleware');
 
-
-const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Ecommerce API',
-            version: '1.0.0',
-            description: 'API documentation for the Ecommerce application',
-        },
-        servers: [
-            {
-                url: 'http://localhost:3000',
-            },
-        ],
-    },
-    apis: ['./routes/productRouter.js'],
-};
-
-const specs = swaggerJsdoc(options);
-
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(specs));
 const {
-    createProduct, 
+    createProduct,
     getProductById,
     getAllProducts,
     updateProduct,
     deleteProduct,
     addToWishList,
-    removeFromWishList
+    removeFromWishList,
 } = require('../controllers/productController');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: API endpoints for managing products
+ */
 
+/**
+ * @swagger
+ * /create:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: Successfully created a new product
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/create', authMiddleware, createProduct);
-// router.post('/create', authMiddleware, isAdmin, createProduct);
+
+/**
+ * @swagger
+ * /all:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved all products
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/all', authMiddleware, getAllProducts);
 
+/**
+ * @swagger
+ * /{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved the product
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/:id', authMiddleware, getProductById);
+
+/**
+ * @swagger
+ * /{id}/addToWishList:
+ *   post:
+ *     summary: Add a product to the wishlist
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully added the product to the wishlist
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/:id/addToWishList', authMiddleware, addToWishList);
+
+/**
+ * @swagger
+ * /{id}/removeFromWishList:
+ *   post:
+ *     summary: Remove a product from the wishlist
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully removed the product from the wishlist
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/:id/removeFromWishList', authMiddleware, removeFromWishList);
 
-
+/**
+ * @swagger
+ * /{id}:
+ *   put:
+ *     summary: Update a product by ID
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: Successfully updated the product
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
 router.put('/:id', authMiddleware, isAdmin, updateProduct);
+
+/**
+ * @swagger
+ * /{id}:
+ *   delete:
+ *     summary: Delete a product by ID
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the product
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully deleted the product
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
 router.delete('/:id', authMiddleware, isAdmin, deleteProduct);
 
-module.exports = router
+module.exports = router;
