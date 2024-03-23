94007189f03cfb3915370215d6f42c5f2d994f0c
shazaaly <shaza.aly@gmail.com>
2024-03-23 04:49:11 +0200

    Add fs module and create directory for resized images

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -1,6 +1,8 @@
 const multer = require('multer');
 const sharp = require('sharp');
 const path = require('path');
+const fs = require('fs');
+
 
 // Set up storage engine
 const multerStorage = multer.diskStorage({
@@ -27,21 +29,29 @@ const uploadPhoto = multer({
     limits: { fieldSize: 20_000_000 },
 });
 
-const resizeImage = async (req, res, next) => {
+
+const resizeImage = async(req, res, next) => {
     if (!req.files) return next();
     try {
         await Promise.all(req.files.map(async file => {
+            const dir = path.resolve(file.destination, 'resized');
+
+            // Create the directory if it does not exist
+            if (!fs.existsSync(dir)){
+                fs.mkdirSync(dir, { recursive: true });
+            }
+
             await sharp(file.path)
-                .resize(500, 500)
-                .toFormat('jpeg')
-                .jpeg({ quality: 90 })
-                .toFile(path.resolve(file.destination, 'resized', file.filename));
+            .resize(500, 500)
+            .toFormat('jpeg')
+            .jpeg({ quality: 90 })
+            .toFile(path.resolve(dir, file.filename));
         }));
     } catch (error) {
         return next(error); // Proper error handling
     }
     next();
-};
+}
 
 // Export the multer upload configuration and resizeImage function
 module.exports = { uploadPhoto, resizeImage };
