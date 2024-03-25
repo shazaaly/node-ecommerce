720fa9c07fcefd2b6f4a9b440cc56569e4a7f18b
shazaaly <shaza.aly@gmail.com>
2024-03-25 02:30:07 +0200

    Add functionality to addToCart API

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -7,12 +7,30 @@ const addToCart = expressAsyncHandler(async (req, res) => {
     if (!user) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
-    const {qty, product} = req.body;
+    const { qty, product } = req.body;
     if (!qty || !product) {
         return res.status(400).json({ message: 'Invalid request' });
     }
-    
-
+    const _id = product._id
+    if (!_id) {
+        return res.status(400).json({ message: 'Invalid request' });
+    }
+    const productExist = await Product.findById(_id);
+    if (!productExist) {
+        return res.status(404).json({ message: 'Product not found' });
+    }
+    const cart = await Cart.findOne({ user: user._id });
+    if (cart) {
+        //check if product exist in cart
+        const existingItem = cart.cartItems.find((item) => item.product === _id)
+        if (existingItem) {
+            existingItem.qty += qty;
+        } else {
+            cart.cartItems.push({ qty, product: _id });
+        }
+        const updatedCart = await cart.save();
+        return res.status(201).json({ message: 'Product added to cart', cart: updatedCart });
+    }
 
 })
 
