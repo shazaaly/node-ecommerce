4895dce8c710c8d30d3f45a7fafd553db8c93a6a
shazaAly <shaza.aly@gmail.com>
2024-04-04 01:59:31 +0200

    Remove duplicate route definition in emailRouter.js

--- routes/emailRouter.js
+++ routes/emailRouter.js
@@ -3,32 +3,6 @@ const router = express.Router();
 
 const sendMail = require('../controllers/emailController');
 
-/**
- * @swagger
- * /send:
- *   post:
- *     summary: Send email.
- *     description: Sends an email.
- *     responses:
- *       200:
- *         description: Email sent successfully.
- *       500:
- *         description: Internal server error.
- */
-router.post('/send', sendMail);
-
-/**
- * @swagger
- * /send:
- *   post:
- *     summary: Send email.
- *     description: Sends an email.
- *     responses:
- *       200:
- *         description: Email sent successfully.
- *       500:
- *         description: Internal server error.
- */
 router.post('/send', sendMail);
 
 module.exports = router;
