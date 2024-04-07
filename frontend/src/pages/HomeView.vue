829f8b5bf1bc7ce2e75a64c69aed7be777546c8b
hamzahdili2001 <hdilihamza955@gmail.com>
2024-04-07 23:09:27 +0000

    add header and hero page (not responsive)

--- frontend/src/pages/HomeView.vue
+++ frontend/src/pages/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import AppNavigation from '@/components/AppNavigation.vue';
+import AppHero from "@/components/AppHero.vue"
 </script>
 
 <template>
   <AppNavigation />
+  <AppHero />
 </template>
